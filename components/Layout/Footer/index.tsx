abfe844a40edc831f2e046c4f507c5a8e101c664
shimphillip <shimphillip@gmail.com>
2020-04-06 23:31:08 -0500

    add footer

---
+++ components/Layout/Footer/index.tsx
@@ -0,0 +1,28 @@
+const Footer: React.FC = (): JSX.Element => {
+  return (
+    <footer>
+      <p>
+        &copy; <time>{new Date().getFullYear()}</time>{' '}
+        <a href="https://www.linkedin.com/in/phillipshim/" target="_blank">
+          Phillip Shim
+        </a>
+      </p>
+      <style jsx>
+        {`
+          footer {
+            width: 100%;
+            text-align: center;
+            margin-top: 1rem;
+          }
+
+          a {
+            text-decoration: none;
+            color: orange;
+          }
+        `}
+      </style>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,6 +2,7 @@ import Header from './Header';
 import globalStyles from '../styles/globalStyles';
 import Main from './Main';
 import Meta from './Meta';
+import Footer from './Footer';
 
 interface MyProps {
   children: React.ReactNode;
@@ -11,11 +12,12 @@ const Layout: React.FC<MyProps> = ({ children }) => {
   return (
     <div>
       <Meta />
-      <Header></Header>
+      <Header />
       <Main>{children}</Main>
       <style jsx global>
         {globalStyles}
       </style>
+      <Footer />
     </div>
   );
 };
